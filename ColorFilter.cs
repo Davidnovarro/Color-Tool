0f66bb67803d7de370ea4e690d3b87d1e3d02635
davitmk <davitmk@hotmail.com>
2021-10-15 13:01:07 +0400

    Initial Commit

---
+++ ColorFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace GrayscaleMaker
+{
+    public abstract class ColorFilter
+    {
+        public abstract Color Process(Color from);
+    }
+
+    public class EmptyColorFilter : ColorFilter
+    {
+        public override Color Process(Color from) => from;
+    }
+
+    public class GrayscaleColorFilter : ColorFilter
+    {
+        public override Color Process(Color from)
+        {
+            var grayValue = (int)Math.Round(((double)from.R + from.G + from.B) / 3d);
+            return Color.FromArgb(from.A, grayValue, grayValue, grayValue);
+        }
+    }
+
+    public class PerceptualGrayscaleColorFilter : ColorFilter
+    {
+        public override Color Process(Color from) 
+        {
+            var grayValue = (int)Math.Round(from.R * 0.299 + from.G * 0.587 + from.B * 0.114);
+            return Color.FromArgb(from.A, grayValue, grayValue, grayValue);
+        }
+    }
+
+}
---
+++ ColorToolForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace GrayscaleMaker
+{
+    partial class ColorToolForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColorToolForm));
+            this.CaptureButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.FilterComboBox = new System.Windows.Forms.ComboBox();
+            this.colorFilterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CaptureButton
+            // 
+            this.CaptureButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CaptureButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CaptureButton.Location = new System.Drawing.Point(670, 394);
+            this.CaptureButton.Name = "CaptureButton";
+            this.CaptureButton.Size = new System.Drawing.Size(118, 44);
+            this.CaptureButton.TabIndex = 0;
+            this.CaptureButton.Text = "Capture";
+            this.CaptureButton.UseVisualStyleBackColor = true;
+            this.CaptureButton.Click += new System.EventHandler(this.OnCaptureClick);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SaveButton.Location = new System.Drawing.Point(546, 394);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(118, 44);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.OnSaveClick);
+            // 
+            // FilterComboBox
+            // 
+            this.FilterComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.FilterComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FilterComboBox.FormattingEnabled = true;
+            this.FilterComboBox.Location = new System.Drawing.Point(12, 415);
+            this.FilterComboBox.Name = "FilterComboBox";
+            this.FilterComboBox.Size = new System.Drawing.Size(172, 23);
+            this.FilterComboBox.TabIndex = 3;
+            this.FilterComboBox.SelectedIndexChanged += new System.EventHandler(this.FilterComboBoxChanged);
+            // 
+            // colorFilterLabel
+            // 
+            this.colorFilterLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.colorFilterLabel.AutoSize = true;
+            this.colorFilterLabel.Location = new System.Drawing.Point(12, 397);
+            this.colorFilterLabel.Name = "colorFilterLabel";
+            this.colorFilterLabel.Size = new System.Drawing.Size(65, 15);
+            this.colorFilterLabel.TabIndex = 4;
+            this.colorFilterLabel.Text = "Color Filter";
+            // 
+            // ColorToolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.colorFilterLabel);
+            this.Controls.Add(this.FilterComboBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.CaptureButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ColorToolForm";
+            this.Text = "Color Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CaptureButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ComboBox FilterComboBox;
+        private System.Windows.Forms.Label colorFilterLabel;
+    }
+}
+
